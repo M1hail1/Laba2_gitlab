901e2b527d17e34c7efb1c223e4a7c625c6af321
Mihail <izmestev.miha1234@gmail.com>
2025-02-22 18:59:44 +0300

    fixed error

--- cmd/lab/main_test.go
+++ cmd/lab/main_test.go
@@ -1,38 +1,39 @@
 package main
 
 import (
- "testing"
+	"slices"
+	"testing"
 )
 
 func TestMainLogic(t *testing.T) {
- tests := []struct {
-  name     string
-  input    []int
-  expected int
- }{
-  {
-   name:     "Positive numbers",
-   input:    []int{1, 2, 3, 4, 5},
-   expected: 5,
-  },
-  {
-   name:     "Negative numbers",
-   input:    []int{-1, -2, -3, -4, -5},
-   expected: -1,
-  },
-  {
-   name:     "Mixed numbers",
-   input:    []int{-1, 2, 0, -4, 5},
-   expected: 5,
-  },
- }
+	tests := []struct {
+		name     string
+		input    []int
+		expected int
+	}{
+		{
+			name:     "Positive numbers",
+			input:    []int{1, 2, 3, 4, 5},
+			expected: 5,
+		},
+		{
+			name:     "Negative numbers",
+			input:    []int{-1, -2, -3, -4, -5},
+			expected: -1,
+		},
+		{
+			name:     "Mixed numbers",
+			input:    []int{-1, 2, 0, -4, 5},
+			expected: 5,
+		},
+	}
 
- for _, tt := range tests {
-  t.Run(tt.name, func(t *testing.T) {
-   maxVal := slices.Max(tt.input)
-   if maxVal != tt.expected {
-    t.Errorf("slices.Max() = %v, expected %v", maxVal, tt.expected)
-   }
-  })
- }
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			maxVal := slices.Max(tt.input)
+			if maxVal != tt.expected {
+				t.Errorf("slices.Max() = %v, expected %v", maxVal, tt.expected)
+			}
+		})
+	}
 }
