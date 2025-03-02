52f95503cdab38b0f38e9281eb83e31bf731e98f
Mihail <izmestev.miha1234@gmail.com>
2025-03-02 16:12:51 +0300

    lab4_1

--- cmd/lab/main.go
+++ cmd/lab/main.go
@@ -1,30 +1,59 @@
 package main
 
 import (
-	"michail-lab/internal/input"
+	"bufio"
 	"fmt"
+	"os"
 	"slices"
 )
 
 func main() {
-	fmt.Print("Размер массива> ")
 	var arrSize uint
-	_, err := fmt.Scanln(&arrSize)
-	if err != nil {
-		fmt.Println("Ошибка ввода:", err)
-		return
+	var scanner *bufio.Scanner
+
+	// Проверяем, есть ли файл с входными данными
+	if _, err := os.Stat("/app/input.txt"); err == nil {
+		// Чтение данных из файла
+		file, err := os.Open("/app/input.txt")
+		if err != nil {
+			fmt.Println("Ошибка открытия файла:", err)
+			return
+		}
+		defer file.Close()
+		scanner = bufio.NewScanner(file)
+	} else {
+		fmt.Println("Use standart input", err)
+		// Интерактивный ввод
+		scanner = bufio.NewScanner(os.Stdin)
 	}
+
+	// Чтение размера массива
+	fmt.Print("Размер массива> ")
+	if scanner.Scan() {
+		_, err := fmt.Sscan(scanner.Text(), &arrSize)
+		if err != nil {
+			fmt.Println("Ошибка ввода:", err)
+			return
+		}
+	}
+
 	if arrSize < 0 {
 		fmt.Println("Размер массива должен быть положительным")
 		return
 	}
 
-	resultArr, err := input.Input(arrSize)
-	if err != nil {
-		fmt.Println("Ошибка ввода: ", err.Error())
-		return
+	// Чтение элементов массива
+	resultArr := make([]int, arrSize)
+	for i := uint(0); i < arrSize; i++ {
+		fmt.Printf("Элемент %d> ", i+1)
+		if scanner.Scan() {
+			_, err := fmt.Sscan(scanner.Text(), &resultArr[i])
+			if err != nil {
+				fmt.Println("Ошибка ввода:", err)
+				return
+			}
+		}
 	}
 
 	fmt.Println("Максимальное значение: ", slices.Max(resultArr))
-
 }
